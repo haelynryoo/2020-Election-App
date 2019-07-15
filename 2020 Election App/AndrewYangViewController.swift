0d4a9f142246e866ba61d52ec9954bcbabff3754
Girls Code <GWC@cs81-ms-crt-10.local>
2019-07-15 15:37:10 -0400

    Connecting Andrew Yang View Controller with the file

--- 2020 Election App/AndrewYangViewController.swift
+++ 2020 Election App/AndrewYangViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class AndrewYangViewController: UIViewController {
 
-    @IBAction func handleProListPressed(_ sender: Any) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
